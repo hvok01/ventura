b5c9afdc10c6846f741480e92e17477f4a6e119c
hvok01 <leonelgomezrodriguez@hotmail.com>
2023-09-19 11:22:08 -0300

    header wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { ShoppingCartSolidIcon } from '@fluentui/react-icons-mdl2';
+import InstagramIcon from './assets/InstagramIcon';
+import FacebookIcon from './assets/FacebookIcon';
+import TwitterIcon from './assets/TwitterIcon';
 
 function App() {
-  const [count, setCount] = useState(0)
+  //https://ventura-fluid-demo.squarespace.com/
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="header">
+          <nav className="navbar">
+            <button className="nav-hamburger">
+              <div className="nav-hamburger-1"></div>
+              <div className="nav-hamburger-2"></div>
+            </button>
+
+            <div className="nav-logo">
+              <span className="site-title">
+                <a href="#">
+                  Ventura
+                </a>
+              </span>
+            </div>
+
+            <div className="nav-links">
+              <ul>
+                <li><a href="#">Shop</a></li>
+                <li><a href="#">Our Story</a></li>
+                <li><a href="#">Journal</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+
+            <div className="nav-socials-purchase-container">
+
+              <div className="nav-socials">
+                <a href="#">
+                  <InstagramIcon />
+                </a>
+                <a href="#">
+                  <FacebookIcon />
+                </a>
+                <a href="#">
+                  <TwitterIcon />
+                </a>
+              </div>
+
+              <div className="nav-purchase-icon">
+                <ShoppingCartSolidIcon style={{color: "#fff", fontSize:"1.5rem"}}/>
+                <div className="nav-purchase-icon-count">
+                  0
+                </div>
+              </div>
+              
+            </div>
+          </nav>
+        </header>
     </>
   )
 }
